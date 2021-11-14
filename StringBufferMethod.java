182c902ba4337fbc220a770486f56d921b6dcc8f
sandeep <84696924+A-41-sandeep@users.noreply.github.com>
2021-11-14 08:46:42 -0600

    Add files via upload

---
+++ StringBufferMethod.java
@@ -0,0 +1,12 @@
+package JavaClassPr;
+
+public class StringBufferMethod {
+    public static void main(String[] args) {
+        StringBuffer s=new StringBuffer("sandeep");
+        System.out.println(s.length());
+        System.out.println(s.capacity());
+        System.out.println(s.append('k'));
+        
+    }
+
+}
