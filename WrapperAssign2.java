82593de90ed8e55919efd41eff548ac782427af1
sandeep <84696924+A-41-sandeep@users.noreply.github.com>
2021-11-14 08:49:16 -0600

    Add files via upload

---
+++ WrapperAssign2.java
@@ -0,0 +1,11 @@
+package JavaClassPr;
+
+import org.jetbrains.annotations.NotNull;
+
+public class WrapperAssign2 {
+    public static void main(String... args) {
+        int i=Integer.parseInt(args[0]);
+        System.out.println(Integer.toBinaryString(i));
+    }
+}
+
