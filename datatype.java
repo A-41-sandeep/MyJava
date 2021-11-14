5fe01e5795f310bc1dce106f335478de8f8dcecc
sandeep <84696924+A-41-sandeep@users.noreply.github.com>
2021-11-14 08:45:13 -0600

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class datatype {
+    public static void main(String[] arg)
+    {
+        int a=10;
+        int b=017;
+        int c= 0x17;
+        float f=0b1101;
+        char ch='\u0061';
+        /*unicode=(\ u+hexadecimal(4digits))*/
+        char s = 's';
+        char s1= 125;
+        System.out.println(s1<s);
+        System.out.println(Integer.toString(a,10)+10);
+
+    }
+}
